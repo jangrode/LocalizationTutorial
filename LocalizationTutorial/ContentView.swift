517c0e4b802f8787c4867ea37ef1d938f390715b
Jan Grodecki <jan.grodecki@rws.com>
2020-09-22 15:33:07 -0700

    Initial Commit

---
+++ LocalizationTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalizationTutorial
+//
+//  Created by Jan Grodecki on 9/22/20.
+//  Copyright © 2020 Jan Grodecki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
